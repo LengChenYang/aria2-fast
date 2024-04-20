fd9e5eeff10d0c9838a52a4713f15b41665cb657
lcy <lingdian068@gmail.com>
2024-04-20 12:02:13 +0800

    Add files via upload

---
+++ x8/patch_dockerfile.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+sed -i 's#cd aria2#cd aria2 \&\& curl -L -o aria2-fast.patch https://raw.githubusercontent.com/hosxy/aria2-fast/lcy-1.37.0/x8/aria2-fast.patch \&\& patch -Np1 < ./aria2-fast.patch#' Dockerfile.mingw
+
