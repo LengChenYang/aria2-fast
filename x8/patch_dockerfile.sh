10faf3108b3f5a4ed60eac3db288157fd1e1cebf
LengChenYang <lingdian068.gmial.com>
2024-06-09 07:06:29 +0800

    dav

--- x8/patch_dockerfile.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-sed -i 's#cd aria2#cd aria2 \&\& curl -L -o aria2-fast.patch https://raw.githubusercontent.com/LengChenYang/aria2-fast/lcy-1.37.0/x8/aria2-fast.patch \&\& patch -Np1 < ./aria2-fast.patch#' Dockerfile.mingw
-
